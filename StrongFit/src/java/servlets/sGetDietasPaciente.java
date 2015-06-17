e98b5ed4de1de2f3718fe7662180ce08fde9c2e8
Ian Mendoza Jaimes <ianmj2013@gmail.com>
2015-06-17 10:53:34 -0500

    dietas

--- StrongFit/src/java/servlets/sGetDietasPaciente.java
+++ StrongFit/src/java/servlets/sGetDietasPaciente.java
@@ -6,13 +6,24 @@
 
 package servlets;
 
+import clases.cCifrado;
+import clases.cConexion;
+import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -31,19 +42,44 @@ public class sGetDietasPaciente extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, SQLException, Exception {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet sGetDietasPaciente</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet sGetDietasPaciente at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+            HttpSession sesion = request.getSession();
+            ArrayList<dietaAsociada>lista = new ArrayList<>();
+            
+            cCifrado seguro = new cCifrado();
+            seguro.AlgoritmoAES();
+            
+            String idOtro = request.getParameter("idOtro");
+                   idOtro = seguro.encriptar(idOtro);
+                   
+            cConexion conecta = new cConexion();
+            conecta.conectar();
+            
+            ResultSet idP = conecta.spTraerIdPaciente(idOtro);
+            int idPaciente = 0;
+            if(idP.next()){
+                idPaciente = idP.getInt("idPaciente");
+            }
+            
+            ResultSet rs = conecta.spGetAsociaciones(idPaciente);
+            String nombreN = "", nombreD = "";
+            int idD = 0;
+            while(rs.next()){
+                nombreD = rs.getString("nombre");
+                nombreN = seguro.desencriptar(rs.getString("creador"));
+                idD = rs.getInt("idDieta");
+                
+                lista.add(new dietaAsociada(idD, nombreN, nombreD));
+            }
+            
+            Map<String, Object> mapa = new HashMap<>();
+            mapa.put("paciente", idPaciente);
+            mapa.put("asociadas", lista);
+            
+            write(response, mapa);
         }
     }
 
@@ -59,7 +95,11 @@ public class sGetDietasPaciente extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(sGetDietasPaciente.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -73,7 +113,11 @@ public class sGetDietasPaciente extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(sGetDietasPaciente.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -86,4 +130,23 @@ public class sGetDietasPaciente extends HttpServlet {
         return "Short description";
     }// </editor-fold>
 
+    private class dietaAsociada{
+        private int idDieta;
+        private String nombreN;
+        private String nombreD;
+        
+        public dietaAsociada(int idDieta, String nombreN, String nombreD){
+            this.idDieta = idDieta;
+            this.nombreN = nombreN;
+            this.nombreD = nombreD;
+        }
+    }
+    
+    private void write(HttpServletResponse response, Map<String, Object> map) throws IOException 
+    {
+        response.setContentType("aplication/json");
+        response.setCharacterEncoding("charset=UTF-8");
+        response.getWriter().write(new Gson().toJson(map));
+    }
+    
 }
